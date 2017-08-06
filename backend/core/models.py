c8ec98853c1c8d3d580273740210a69bf06d4575
Christopher Dignam <chris@dignam.xyz>
2017-08-06 12:37:45 -0400

    ADD: Customer user model
    - Users will sign in with email and password. No username required.

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a user with given email and password.
+        """
+        if not email:
+            raise ValueError('User must have an email address')
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(email=email, password=password)
+        user.is_admin = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that only requires an email and password"""
+    email = models.EmailField(unique=True)
+
+    # required for admin
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    created = models.DateField(auto_now_add=True)
+    last_updated = models.DateField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = MyUserManager()
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    # required for admin
+    def has_perm(self, perm, obj=None):
+        """Does the user have a specific permission?"""
+        # TODO: Add permissions
+        return True
+
+    # required for admin
+    def has_module_perms(self, app_label):
+        """Does the user have permissions to view the app `app_label`?"""
+        # TODO: Add permissions
+        return True
+
+    # required for admin
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def __str__(self):
+        return self.email
