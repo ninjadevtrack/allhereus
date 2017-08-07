67c2b376a6dbe92a35dbf583d75bb9f8555c1bae
Christopher Dignam <chris@dignam.xyz>
2017-08-06 21:45:30 -0400

    FIX: PEP8 admin.py

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -6,6 +6,7 @@ from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 from core.models import MyUser, Student, CheckIn, District, School, Team
 
+
 # https://github.com/django/django/blob/a96b981d84367fd41b1df40adf3ac9ca71a741dd/django/contrib/auth/forms.py#L64-L150
 class UserCreationForm(forms.ModelForm):
     """
@@ -42,10 +43,11 @@ class UserChangeForm(forms.ModelForm):
     password hash display field.
     """
     # https://stackoverflow.com/a/15630360/3555105
-    password = ReadOnlyPasswordHashField(label= ("Password"),
-        help_text= ("Raw passwords are not stored, so there is no way to see "
-                    "this user's password, but you can change the password "
-                    "using <a href=\'../password/\'>this form</a>."))
+    password = ReadOnlyPasswordHashField(
+        label="Password",
+        help_text=("Raw passwords are not stored, so there is no way to see "
+                   "this user's password, but you can change the password "
+                   "using <a href=\'../password/\'>this form</a>."))
 
     class Meta:
         model = MyUser
@@ -56,6 +58,7 @@ class UserChangeForm(forms.ModelForm):
         # This is done here, rather than on the field, because the
         # field does not have access to the initial value
         return self.initial["password"]
+
 
 # https://github.com/django/django/blob/8346680e1ca4a8ddc8190baf3f5f944f6418d5cf/django/contrib/auth/admin.py#L42-L207
 class UserAdmin(BaseUserAdmin):
@@ -69,7 +72,7 @@ class UserAdmin(BaseUserAdmin):
         (None, {'fields': ('email', 'password', 'image_tag')}),
         ('Personal Info', {'fields': ('first_name', 'last_name',)}),
         ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser',
-                                       'groups', 'user_permissions')}),
+                                    'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'last_updated', 'date_joined')})
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
@@ -77,8 +80,7 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2')}
-        ),
+            'fields': ('email', 'password1', 'password2')}),
     )
     # The forms to add and change user instances
     form = UserChangeForm
