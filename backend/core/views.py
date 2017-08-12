146e3bcaccd7989298429b6948f6cc5b8b33b251
Christopher Dignam <chris@dignam.xyz>
2017-08-12 09:16:39 -0400

    FIX: PEP8

--- backend/core/views.py
+++ backend/core/views.py
@@ -56,6 +56,7 @@ def forgotpassword(request):
     """
     return render(request, 'core/forgotpassword.html')
 
+
 @login_required
 def profile(request):
     """
@@ -72,6 +73,7 @@ def profile(request):
         'recent_checkins': CheckIn.objects.order_by('created_on').all()[:10]
     }
     return render(request, 'core/profile.html', context)
+
 
 @login_required
 def profile_edit(request):
@@ -90,15 +92,16 @@ def profile_edit(request):
     }
     return render(request, 'core/profile_edit.html', context)
 
+
 @login_required
 def checkins(request):
     """
     list all the checkins for teacher
     """
 
-    if request.user.role=='DA':
+    if request.user.role == 'DA':
         checkins = CheckIn.objects.filter(student__group=request.user.district)
-    elif request.user.role=='SA':
+    elif request.user.role == 'SA':
         checkins = CheckIn.objects.filter(student__school=request.user.school)
     else:
         checkins = CheckIn.objects.filter(student__in=request.user.student_set.all())
@@ -124,6 +127,7 @@ def checkins_add(request):
 
     return render(request, 'core/checkin_edit.html', {'form': form})
 
+
 @login_required
 def checkin(request, id):
     """
@@ -133,8 +137,8 @@ def checkin(request, id):
     return render(request, 'core/checkin.html', {
         'checkin': checkin_event,
         'success_score_percentage': checkin_event.success_score / 10 * 100,
-        }
-    )
+    })
+
 
 @login_required
 def checkin_edit(request, id):
@@ -153,6 +157,7 @@ def checkin_edit(request, id):
 
     return render(request, 'core/checkin_edit.html', {'form': form})
 
+
 @login_required
 def teams(request):
     """
@@ -160,6 +165,7 @@ def teams(request):
     Manager: list of all teams of the manager's group
     """
     return render(request, 'core/teams.html')
+
 
 @login_required
 def team(request, id):
