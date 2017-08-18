1b856e80c19ff0763be4c26da10a70302ab2ee08
Christopher Dignam <chris@dignam.xyz>
2017-08-17 22:23:24 -0400

    ADD: Password reset confirm template

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     url(r'^password_change/$', auth_views.PasswordChangeDoneView.as_view(), name='password_change'),
     url(r'^password_change/done/$', auth_views.PasswordChangeView.as_view(), name='password_change_done'),
     url(r'^password_reset/$', auth_views.PasswordResetView.as_view(template_name='core/password_reset.html'), name="password_reset"),
-    url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(template_name='core/password_reset_done.html'), name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
