e0c5924bacef68ebe16126972dc9ae4967713950
Christopher Dignam <chris@dignam.xyz>
2017-08-20 12:19:54 -0400

    FIX: Typo in profile edit page. Fix misc things

--- backend/set_server.py
+++ backend/set_server.py
@@ -1,5 +1,11 @@
+import logging
+
 from django.contrib.sites.models import Site
+
+logger = logging.getLogger(__name__)
+logger.info('Setting server name')
+
 my_site = Site.objects.get(pk=1)
-my_site.domain = 'platform.allhere.com'
+my_site.domain = 'app.allhere.com'
 my_site.name = 'AllHere'
 my_site.save()
