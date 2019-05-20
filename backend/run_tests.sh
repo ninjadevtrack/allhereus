e0a08f00e2dbce8c7fbae7b3b8d31bcf2305491c
Rhett Smith <rsmith@biocio.com>
2019-05-20 18:14:45 -0400

    a2dw-39: wip run tests without kubernetes.

--- backend/run_tests.sh
+++ backend/run_tests.sh
@@ -1,2 +1,6 @@
+# wait for database - https://www.stavros.io/posts/how-deploy-django-docker/
+while ! nc -w 1 -z ${DB_HOST} 5432
+    do sleep 0.1
+done
 python manage.py collectstatic --noinput
 pytest -c pytest.ini --ignore=ednudge-sdk-python
