ec670e80ec3742e4df6243ce211b134668899899
Yo To <yohantoress226@gmail.com>
2019-07-12 06:21:53 -0400

    write test

--- backend/core/test_routes.py
+++ backend/core/test_routes.py
@@ -1053,3 +1053,29 @@ def test_library_anonymous(client):
     # anonymous
     res = client.get('/library/')
     assert res.status_code == 302
+
+"""
+Url Tests url(r'/strategies/$', views.strategies, name='strategies')
+"""
+def test_strategies_teacher(client, teacher):
+    # teacher
+    client.force_login(teacher)
+    res = client.get('/strategies/')
+    assert res.status_code == 200
+
+def test_strategies_school_admin(client, school_admin):
+    # school_admin
+    client.force_login(school_admin)
+    res = client.get('/strategies/')
+    assert res.status_code == 200
+
+def test_strategies_district_admin(client, district_admin):
+    # district_admin
+    client.force_login(district_admin)
+    res = client.get('/strategies/')
+    assert res.status_code == 200
+
+def test_strategies_anonymous(client):
+    # anonymous
+    res = client.get('/strategies/')
+    assert res.status_code == 302
