0fde1067f8634b37e076e7c438cee6977c84cab8
Yo To <yohantoress226@gmail.com>
2019-07-25 21:46:15 -0400

    flatpage configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -68,6 +68,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.flatpages',
     'django.contrib.messages',
 #    'django.contrib.staticfiles',
     'django.contrib.humanize',
